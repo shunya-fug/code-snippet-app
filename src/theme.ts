7f278e983ba5984a5262c07013404e61b45b2229
shunya-fug <shunnya0715@gmail.com>
2023-04-16 18:43:28 +0900

    💄テーマを作成

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,38 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  // TODO:テーマ設定
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#61AFEF",
+    },
+    secondary: {
+      main: "#C678DD",
+    },
+    background: {
+      paper: "#1E1E1E",
+      default: "#121212",
+    },
+    text: {
+      primary: "#D4D4D4",
+      secondary: "#8A8A8A",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "Fira Code",
+      "Roboto Mono",
+      "Menlo",
+      "Monaco",
+      "Consolas",
+      "Ubuntu Mono",
+      "Noto Mono",
+      "Oxygen Mono",
+      "Liberation Mono",
+      "Courier New",
+      "monospace",
+    ].join(","),
+  },
 });
 
 export default theme;
