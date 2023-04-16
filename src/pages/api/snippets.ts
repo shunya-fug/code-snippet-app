b3d3efb7119e683b5dd72c7a02f484f52df01461
shunya-fug <shunnya0715@gmail.com>
2023-04-16 16:19:30 +0900

    🔨API作成

---
+++ src/pages/api/snippets.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const snippets = await prisma.codeSnippet.findMany();
+    res.status(200).json(snippets);
+  } else if (req.method === "POST") {
+    const { title, description, code, language, tags } = req.body;
+    const snippet = await prisma.codeSnippet.create({
+      data: { title, description, code, language, tags },
+    });
+    res.status(200).json(snippet);
+  } else {
+    res.status(404).send("Not found");
+  }
+}
