86833b6f374ab370a4a3129684ead437db58f42e
shunya-fug <shunnya0715@gmail.com>
2023-05-05 00:08:47 +0900

    🔨`prisma-client-js`設定変更に伴う修正
    4ecc4fd0e9cc1121f2453c2c908385403097abc8

--- src/pages/api/snippets.ts
+++ src/pages/api/snippets.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
 
-import { Snippet } from "@/generated/prisma";
+import { PrismaClient, Snippet } from "@/generated/prisma";
 
 const prisma = new PrismaClient();
 
@@ -10,11 +9,11 @@ export default async function handler(
   res: NextApiResponse
 ): Promise<void> {
   if (req.method === "GET") {
-    const snippets = await prisma.codeSnippet.findMany();
+    const snippets = await prisma.snippet.findMany();
     res.status(200).json(snippets);
   } else if (req.method === "POST") {
     const { title, description, code, language, tags } = req.body;
-    const snippet = await prisma.codeSnippet.create({
+    const snippet = await prisma.snippet.create({
       data: <Snippet>{ title, description, code, language, tags },
     });
     res.status(200).json(snippet);
